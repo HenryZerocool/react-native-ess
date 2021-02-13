c473df0b7eddb2e48c9644663216dfa22e4b5d2d
Phan Hieu Nguyen <fanhieu94@gmail.com>
2021-02-13 01:36:27 -0500

    added play again button

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import * as React from 'react';
-import Game from './components/Game'
+import Game from './components/Game';
 
-export default function App() {
-  return (
-    <Game randomNumberCount={8}/>
-  );
+export default class App extends React.Component {
+  state = {
+    gameId : 1,
+  };
+  playNewGame = () => {
+    this.setState((prevState) => ({
+      gameId : prevState.gameId + 1
+    }))
+  }
+  render() {
+    return <Game key={this.state.gameId} onPlayAgain={this.playNewGame} randomNumberCount={8} />;
+  }
 }
--- components/Game.js
+++ components/Game.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import RandomChoice from './RandomChoice';
+
+import shuffle from 'lodash.shuffle';
 
 export default class Game extends React.Component {
   static propTypes = {
@@ -16,7 +18,7 @@ export default class Game extends React.Component {
     this.gameTimerInterval = setInterval(() => {
       this.setState(
         (prevState) => {
-          return {gameTimer: prevState.gameTimer - 1,}
+          return { gameTimer: prevState.gameTimer - 1 };
         },
         () => {
           if (this.state.gameTimer === 0) clearInterval(this.gameTimerInterval);
@@ -24,16 +26,28 @@ export default class Game extends React.Component {
       );
     }, 1000);
   }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      nextState.selectedNoIndexs !== this.state.selectedNoIndexs ||
+      nextState.gameTimer === 0
+    ) {
+      this.gameStatus = this.calcGameStatus(nextState);
+      if (this.gameStatus !== 'PLAYING') clearInterval(this.gameTimerInterval);
+    }
+    return true;
+  }
   componentWillUnmount() {
     clearInterval(this.gameTimerInterval);
   }
+
+  gameStatus = 'PLAYING';
   randomNumberArr = Array.from({ length: this.props.randomNumberCount }).map(
     () => 1 + Math.floor(10 * Math.random())
   );
+  shuffleRandomArr = shuffle(this.randomNumberArr);
   target = this.randomNumberArr
     .slice(0, this.randomNumberArr.length - 2) // only a few numbers in the array needed for sum
     .reduce((acc, cur) => cur + acc, 0); // cal target base on numbers in array
-  // TODO: shuffle the list
 
   isSelected = (numberIndex) => {
     return this.state.selectedNoIndexs.indexOf(numberIndex) >= 0;
@@ -43,39 +57,38 @@ export default class Game extends React.Component {
       selectedNoIndexs: [...prevState.selectedNoIndexs, numberIndex],
     }));
   };
-  gameStatus = () => {
-    const sum = this.state.selectedNoIndexs.reduce(
-      (acc, curr) => acc + this.randomNumberArr[curr],
+  calcGameStatus = (nextState) => {
+    const sum = nextState.selectedNoIndexs.reduce(
+      (acc, curr) => acc + this.shuffleRandomArr[curr],
       0
     );
-    if (sum > this.target || this.state.gameTimer === 0) return 'LOST';
+    if (sum > this.target || nextState.gameTimer === 0) return 'LOST';
     if (sum < this.target) return 'PLAYING';
     if (sum === this.target) return 'WON';
   };
   render() {
-    const gameStatus = this.gameStatus();
     return (
       <View style={styles.container}>
-        <Text style={[styles.target, styles[`STATUS_${this.gameStatus()}`]]}>
+        <Text style={[styles.target, styles[`STATUS_${this.gameStatus}`]]}>
           {this.target}
         </Text>
         <View style={styles.randomContainer}>
-          {this.randomNumberArr.map((number, index) => (
+          {this.shuffleRandomArr.map((number, index) => (
             <RandomChoice
               key={index}
               id={index}
               randomNumber={number}
               isDisable={
-                this.isSelected(index) || this.gameStatus() !== 'PLAYING'
+                this.isSelected(index) || this.gameStatus !== 'PLAYING'
               }
               onPress={this.selectNumber}
             />
           ))}
         </View>
-        <Text>
-          {gameStatus}
-          {this.state.gameTimer}
-        </Text>
+        {this.gameStatus !== 'PLAYING' && (
+          <Button title="Play Again" onPress={this.props.onPlayAgain} />
+        )}
+        <Text>{this.state.gameTimer}</Text>
       </View>
     );
   }
